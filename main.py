d826a0b2178d40ffa6e5db7fd4979506c37b97c2
sent1nu11 <sent1nu11@gmail.com>
2022-05-16 23:19:43 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from tkinter import *
+
+root = tk.Tk()
+root.title("First GUI")
+root.minsize(width=500, height=300)
+
+def getFile():
+    filename = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select file",
+                               filetypes=(("Excel file", "*.xlsx"), ("all files", "*.*")))
+
+class myButton:
+    def __init__(self):
+        self.button = Button(text="press", command=getFile)
+        self.button.pack()
+
+my_button = myButton()
+
+
+root.mainloop()
+
+
+
+
+
+root.mainloop()
